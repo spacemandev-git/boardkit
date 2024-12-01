b6900d7d6fe007dc8c70d23ef192b0ec901c6a78
spacemandev <devbharel@gmail.com>
2024-12-01 05:33:54 -0600

    added pixi canvas

--- src/routes/lobby/[roomId]/+page.svelte
+++ src/routes/lobby/[roomId]/+page.svelte
@@ -3,11 +3,14 @@
     import { joinRoom, selfId } from "trystero/firebase";
     import { onMount } from "svelte";
     import type { Room } from "trystero";
+    import Game from "./game.svelte";
     const config = {
         appId: `https://boardkit-ed83f-default-rtdb.firebaseio.com/`,
     };
 
     let room: Room;
+    let inRoom: boolean = false;
+
     onMount(() => {
         console.log(selfId);
         room = joinRoom(config, $page.params.roomId);
@@ -21,4 +24,14 @@
     {#each Object.keys(room.getPeers()) as peerId}
         <p>{peerId}</p>
     {/each}
+    <div>
+        <h3>Load Module from Zip</h3>
+        <h3>Load Save File for Module</h3>
+    </div>
+
+    <button on:click={() => (inRoom = true)}>Start Room</button>
+{/if}
+
+{#if inRoom}
+    <Game />
 {/if}
---
+++ src/routes/lobby/[roomId]/game.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import * as PIXI from "pixi.js";
+    import { onMount } from "svelte";
+    let canvas: HTMLCanvasElement;
+    let width = 800;
+    let height = 500;
+    onMount(() => {
+        const app = new PIXI.Application();
+        app.init({
+            canvas: canvas,
+            width: width,
+            height: height,
+            backgroundColor: 0x000000,
+            resolution: window.devicePixelRatio,
+        });
+    });
+</script>
+
+<canvas
+    bind:this={canvas}
+    {width}
+    {height}
+    style="width: {width}px; height: {height}px;"
+></canvas>
